7ea88e49ccc5ee614aa52480d0bc8fda0459fe06
jc578271 <lehoang.nguyen@my.jcu.edu.au>
2023-03-10 11:49:30 +0700

    update FLashListV2

---
+++ src/components/bottomSheetScrollable/BottomSheetFlashListV2.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {FlashList, FlashListProps} from '@shopify/flash-list';
+
+import Animated from 'react-native-reanimated';
+import {
+  BottomSheetScrollView,
+  createBottomSheetScrollableComponent,
+  SCROLLABLE_TYPE
+} from "@gorhom/bottom-sheet";
+
+const F2 = React.forwardRef((props: any, ref) => {
+  return (
+    <FlashList
+      ref={ref}
+      {...props}
+      renderScrollComponent={BottomSheetScrollView}
+    />
+  );
+});
+
+const AnimatedFlashList = Animated.createAnimatedComponent<FlashListProps<any>>(
+  F2 as any,
+);
+
+const BottomSheetFlashListComponent = createBottomSheetScrollableComponent(
+  SCROLLABLE_TYPE.FLATLIST,
+  AnimatedFlashList,
+);
+
+const BottomSheetFlashListV2 = React.memo(BottomSheetFlashListComponent);
+BottomSheetFlashListV2.displayName = 'BottomSheetFlashListV2';
+
+export default BottomSheetFlashListV2 as <T>(
+  props: FlashListProps<T>
+) => ReturnType<typeof FlashList>;
